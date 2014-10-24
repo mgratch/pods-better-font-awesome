b3d80291aaa74f3fd1d2e97db1d3fc76f177ace8
mgratch <mgratch@liveworksink.com>
2014-10-24 18:27:24 -0500

    simplify to determine what I am doing wrong

--- pods-better-font-awesome.php
+++ pods-better-font-awesome.php
@@ -19,65 +19,17 @@
  * GitHub Plugin URI: https://github.com/mgratch/pods-better-font-awesome/
  */
 
-add_action( 'plugins_loaded', 'lwi_pods_load_bfa',10 );
-/** 
- * Initialize the Better Font Awesome Library.
- */
-function lwi_pods_load_bfa() {
+require_once ( dirname( __FILE__ ) . '/better-font-awesome-library/better-font-awesome-library.php' );
+// Initialize the library with custom args.
+Better_Font_Awesome_Library::get_instance();
 
-    // Include the main library file. Make sure to modify the path to match your directory structure.
-    require_once ( dirname( __FILE__ ) . '/better-font-awesome-library/better-font-awesome-library.php' );
+// Get the active Better Font Awesome Library Object.
+$my_bfa = Better_Font_Awesome_Library::get_instance();
 
-    // Set the library initialization args (defaults shown).
-    $args = array(
-            'version'                 => 'latest',
-            'minified'                => true,
-            'remove_existing_fa'      => true,
-            'load_styles'             => true,
-            'load_admin_styles'       => true,
-            'load_shortcode'          => true,
-            'load_tinymce_plugin'     => true
-    );
-
-    // Initialize the Better Font Awesome Library.
-    // Initialize the library with custom args.
-    Better_Font_Awesome_Library::get_instance( $args );
-
-    // Get the active Better Font Awesome Library Object.
-    $my_bfa = Better_Font_Awesome_Library::get_instance($args);
-
-    // Get info on the Better Font Awesome Library object.
-    $version = $my_bfa->get_version();
-    $stylesheet_url = $my_bfa->get_stylesheet_url();
-    $prefix = $my_bfa->get_prefix();
-    $icons = $my_bfa->get_icons();
-
-    //Dump BFA Library Object - This Works
-    var_dump(Better_Font_Awesome_Library::get_instance( $args ));
-
-    //Break
-    echo '<br><hr><br>';
-
-    //Dump BFA Library Object - This Works
-    var_dump($my_bfa);
-
-    //Break
-    echo '<br><hr><br>';
-    
-    //Dump specific info from the Better Font Awesome Library Object - This Doesn't Work
-    var_dump($stylesheet_url);
-    
-    // Output all available icons. - This Doesn't Work
-    foreach ( $icons as $icon ) {
-        echo $icon . '<br />';
-    }
-
-
-
-}
-
-if (!is_admin()):
-    //do something
-endif;
+// Get info on the Better Font Awesome Library object.
+var_dump($my_bfa->get_version());
+var_dump($my_bfa->get_stylesheet_url());
+var_dump($my_bfa->get_prefix());
+var_dump($my_bfa->get_icons());
 
 ?>
